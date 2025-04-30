82e953ac641fcdbe5c0958aa613d817c504e5f6f
nmagaru <nicholas.magaru@gmail.com>
2025-04-30 09:00:07 -0500

    created transaction and file manager classes

---
+++ Capstone1AccountingLedger/src/main/java/org/example/FileManager.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+    public static void writeTransaction (Transaction transaction) {
+        String filePath = "src/main/resources/transactions.csv";
+        File file = new File(filePath);
+
+        try {
+            //check if folder exists
+            File folder = file.getParentFile();
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            //check if file exists, create new file if no
+            boolean fileExists = file.exists();
+            if (!fileExists) {
+                try {
+                    if (file.createNewFile()) {
+                        System.out.println("File created: " + file.getName());
+                    }
+                } catch (IOException e) {
+                    System.out.println("File could not be created.");
+                    e.printStackTrace();
+                }
+            }
+
+            //check if file is empty
+            boolean fileEmpty = file.length() == 0;
+            FileWriter writer = new FileWriter(file, true);
+
+            //write to file
+            //write header if empty, write transaction if no
+            if (fileEmpty) {
+                writer.write("date|time|description|vendor|amount\n");
+            }
+            writer.write(transaction.toString() + "\n");
+
+            //close writer
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Transaction could not be written to file.");
+            e.printStackTrace();
+        }
+    }
+}
